6880119974a3f67034a5a2bf2857b2adca6288c2
Nicolas D'Amelio <nicolas@global66.com>
2019-11-16 23:53:03 -0300

    some fix to return forbbiden

--- src/main/java/com/ndamelio/mutant/boundary/MutantBoundary.java
+++ src/main/java/com/ndamelio/mutant/boundary/MutantBoundary.java
@@ -1,9 +1,11 @@
 package com.ndamelio.mutant.boundary;
 
+import com.ndamelio.mutant.config.MutantForbiddenException;
 import com.ndamelio.mutant.dao.DNASequence;
 import com.ndamelio.mutant.controller.MutantController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -12,8 +14,6 @@ import javax.validation.Valid;
 @RequestMapping("/mutant")
 public class MutantBoundary {
 
-    private final static Logger LOG = LoggerFactory.getLogger(MutantBoundary.class);
-
     private final MutantController mutantController;
 
     public MutantBoundary(MutantController mutantController) {
@@ -21,8 +21,10 @@ public class MutantBoundary {
     }
 
     @PostMapping
-    public Object checkDNA(@RequestBody @Valid DNASequence dnaSequence) throws Exception {
-        return mutantController.isMutant(dnaSequence.getDna());
+    @ResponseStatus(value = HttpStatus.OK)
+    public void checkDNA(@RequestBody @Valid DNASequence dnaSequence) throws Exception {
+        boolean result = mutantController.isMutant(dnaSequence.getDna());
+        if (!result) throw new MutantForbiddenException("Human DNA");
     }
 
     @GetMapping("/stats")
